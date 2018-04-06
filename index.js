2c960fc21137e75c0d5ce770edf7fff95d1ced01
davidnamills <mullerlight73@hotmail.com>
2018-04-06 18:34:54 +0800

    basic auth implemented

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+require('./services/passport');
+
 const express = require('express') ;
+
+const PORT = process.env.PORT ||  5000;
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+require('./routes/authRoutes')(app);
 
-app.get('/', (req, res)=>{
-    res.send({hi: 'there'});
-});
 
 app.listen(PORT, ()=>{
     console.log(`listening on port: ${PORT}`);
-})
+});
